0d15ea4e9805f9ab6f403289a98cfd9020d9aa61
Vasily Vavilov <vasily.vavilov+Americor@americor.com>
2025-11-01 16:54:18 +0300

    Laucher for homeworks

---
+++ launcher.py
@@ -0,0 +1,10 @@
+import Disemvowel_Trolls
+result = "Result = " + Disemvowel_Trolls.disemvowel('Hello')
+#print(result)
+
+import Even_or_Odd
+print("8 " + Even_or_Odd.even_or_odd(8))
+print(Even_or_Odd.even_or_odd(1))
+print(Even_or_Odd.even_or_odd(2))
+print(Even_or_Odd.even_or_odd(4))
+print(Even_or_Odd.even_or_odd(6))
